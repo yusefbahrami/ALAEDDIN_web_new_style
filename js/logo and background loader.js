5624d8aaf0cf362d9fed7619f3e7dffa425a8085
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-01 23:40:23 +0330

    styles completed - responsive in progress

--- js/logo and background loader.js
+++ js/logo and background loader.js
@@ -1,11 +1,12 @@
-const imgProfile = document.querySelectorAll(".profile-image");
-
 window.addEventListener("load", async () => {
+  const imgProfile = document.querySelector(".profile-image");
+  const imgProfileRoute = document.querySelector(".profile-image-route");
+  const main = document.querySelector(".main");
   const result = await fetch("./assets/IMG_20221024_160602_501.jpg");
   if (result.status == 200) {
-    imgProfile[0].setAttribute("src", result.url);
-    imgProfile[1].setAttribute("href", result.url);
-    document.body.style.background = `linear-gradient(rgba(13, 71, 83, 78%), rgba(13, 71, 83, 78%)),
-    url("./assets/IMG_20221024_160602_501.jpg") center center/cover fixed`;
+    imgProfile.setAttribute("src", result.url);
+    imgProfileRoute.setAttribute("href", result.url);
+    main.style.background = `linear-gradient(rgba(68, 68, 68, 0%),rgba(23, 23, 23, 100%)),
+    url(${result.url}) center center/cover fixed`;
   }
 });
