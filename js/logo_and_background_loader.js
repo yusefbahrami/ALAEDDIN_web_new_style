b637a3e2e588bafb7969550030f81b4c9d7477fc
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-04 15:15:15 +0330

    light mode in progress

--- js/logo_and_background_loader.js
+++ js/logo_and_background_loader.js
@@ -1,12 +1,15 @@
-window.addEventListener("load", async () => {
+const setImages = async () => {
   const imgProfile = document.querySelector(".profile-image");
   const imgProfileRoute = document.querySelector(".profile-image-route");
   const main = document.querySelector(".main");
-  const result = await fetch("./assets/IMG_20221024_160602_501.jpg");
+  var result = await fetch("./assets/IMG_20221024_160602_501.jpg");
   if (result.status == 200) {
     imgProfile.setAttribute("src", result.url);
     imgProfileRoute.setAttribute("href", result.url);
     main.style.background = `linear-gradient(rgba(68, 68, 68, 20%),rgba(23, 23, 23, 100%)),
-    url(${result.url}) center center/cover fixed`;
+  url(${result.url}) center center/cover fixed`;
   }
+};
+window.addEventListener("load", () => {
+  setImages();
 });
--- js/setDarkLight.js
+++ js/setDarkLight.js
@@ -1,11 +1,14 @@
 const darkLight = document.getElementById("dark-light");
 
-darkLight.addEventListener("click", () => {
+darkLight.addEventListener("click", (e) => {
+  e.preventDefault();
   const main = document.getElementById("main");
   main.classList.toggle("light");
   if (main.classList.contains("light")) {
     darkLight.setAttribute("class", "uil uil-moon");
+    document.body.setAttribute("class", "lightBody");
   } else {
     darkLight.setAttribute("class", "uil uil-sun");
+    document.body.setAttribute("class", "darkBody");
   }
 });
