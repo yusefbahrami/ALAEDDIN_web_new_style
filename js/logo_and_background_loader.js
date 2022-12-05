082f8800a42cfe7613f5d33cb3f4a8da09126fd8
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-12-05 15:38:09 +0330

    background image change in css

--- js/logo_and_background_loader.js
+++ js/logo_and_background_loader.js
@@ -2,21 +2,15 @@ const setImages = async () => {
   const imgProfile = document.querySelector(".profile-image");
   const imgProfileRoute = document.querySelector(".profile-image-route");
   const main = document.querySelector(".main");
-  let gradiant;
   let result;
   if (main.classList.contains("light")) {
     result = await fetch("./assets/yusef.jpg");
-    gradiant = "rgba(239, 255, 253, 100%)";
   } else {
     result = await fetch("./assets/IMG_20221024_160602_501.jpg");
-    gradiant = "rgba(23, 23, 23, 100%)";
   }
   if (result.status == 200) {
     imgProfile.setAttribute("src", result.url);
     imgProfileRoute.setAttribute("href", result.url);
-    main.style.background = `linear-gradient(rgba(68, 68, 68, 20%),${gradiant}),
-      url(${result.url}) center center/cover fixed`;
   }
 };
 export default setImages;
-
